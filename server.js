c3294a10818b197b11d7d2a26ac22816c267ab74
Maciej Laskowski <maciek30088@gmail.com>
2023-08-18 22:48:06 +0100

    Add handlebars templates

--- server.js
+++ server.js
@@ -1,35 +1,48 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('express-handlebars');
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.show = (name) => {
-    res.sendFile(path.join(__dirname, `/views/${name}`));
-  };
-  next();
-});
+app.engine('.hbs', hbs());
+app.set('view engine', '.hbs');
 
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.use('/user', (req, res, next) => {
-  res.show('login.html');
+  res.render('login');
 });
 
 app.get('/', (req, res) => {
-  res.show('index.html');
+  res.render('index');
 });
 
 app.get('/home', (req, res) => {
-  res.show('index.html');
+  res.render('index');
 });
 
 app.get('/about', (req, res) => {
-  res.show('about.html');
+  res.render('about', { layout: 'dark' });
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact');
+});
+
+app.get('/info', (req, res) => {
+  res.render('info');
+});
+
+app.get('/history', (req, res) => {
+  res.render('history');
+});
+
+app.get('/hello/:name', (req, res) => {
+  res.render('hello', {  name: req.params.name });
 });
 
 app.use((req, res) => {
-  res.status(404).show('notFound.html');
+  res.status(404).render('notFound');
 });
 
 app.listen(8000, () => {
