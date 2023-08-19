3bbe700592bd1978670923d8af061cbbe5a2ef6d
Maciej Laskowski <maciek30088@gmail.com>
2023-08-19 20:19:05 +0100

    Add contact form

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('express-handlebars');
-
+const multer = require('multer');
 const app = express();
 
 app.engine('.hbs', hbs());
 app.set('view engine', '.hbs');
 
+
+
 app.use(express.static(path.join(__dirname, '/public')));
+
+app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
+// Save uploaded files
+// app.use(multer({ dest: 'uploads/' }).single('file'));
+
+app.use(
+  multer({
+    storage: multer.memoryStorage(),
+  }).single('file')
+);
 
 app.use('/user', (req, res, next) => {
   res.render('login');
@@ -39,6 +52,22 @@ app.get('/history', (req, res) => {
 
 app.get('/hello/:name', (req, res) => {
   res.render('hello', {  name: req.params.name });
+});
+
+
+app.post('/contact/send-message', (req, res) => {
+  const { author, sender, title, message } = req.body;
+
+  if (author !== '' && sender !== '' && title !== '' && message !== '') {
+    if (req.file) {
+      const fileName = req.file.originalname;
+      res.render('contact', { isSent: true, fileName });
+    } else {
+      res.render('contact', { isError: true, fileError: true });
+    }
+  } else {
+    res.render('contact', { isError: true });
+  }
 });
 
 app.use((req, res) => {
